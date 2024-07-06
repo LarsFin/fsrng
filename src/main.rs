6ccf141172927497f57c2036883fcf10c51a98bc
LarsFin <larsfinlay@googlemail.com>
2024-07-06 19:43:56 +0100

    refactor

--- src/main.rs
+++ src/main.rs
@@ -3,7 +3,7 @@ use fsrng::*;
 fn main() {
     let game = String::from("demons_souls");
     let mut routes = data::load_routes(game).unwrap();
-    let mut rng = gen_rng(10);
+    let mut rng = gen_rng(11);
 
     let selected_route = routes.items.remove(0);
     let generated_route = determine_objective_order(selected_route, &mut rng);
