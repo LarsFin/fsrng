e0c73adaf5007ce668cc20c7eaa5faa5f5f28214
LarsFin <larsfinlay@googlemail.com>
2024-07-08 19:12:59 +0100

    removed leftover code

--- src/data/models.rs
+++ src/data/models.rs
@@ -17,110 +17,40 @@ pub struct Game {
 #[derive(Deserialize)]
 pub struct RouteSchema {
     pub file_version: String,
-    pub routes: Vec<_Route>,
-    pub objectives: Vec<_Objective>,
-}
-
-#[derive(Deserialize)]
-pub struct _Route {
-    pub id: String,
-    pub info: _RouteInfo,
-}
-
-#[derive(Deserialize, Serialize)]
-pub struct RouteInfo {
-    pub name: String,
-    pub description: String,
-}
-
-#[derive(Deserialize)]
-pub struct _Objective {
-    pub id: String,
-    pub info: _ObjectiveInfo,
-    pub excluded_routes: Vec<String>,
-    pub weighting: HashMap<String, u64>,
-    pub condition: Option<_Condition>,
-}
-
-pub struct _WeightedObjective {
-    pub id: String,
-    pub weight: u64,
-}
-
-#[derive(Deserialize, Serialize, Clone)]
-pub struct _ObjectiveInfo {
-    pub name: String,
-    pub location: String,
-}
-
-#[derive(Deserialize)]
-#[serde(tag = "type")]
-pub enum _Condition {
-    Branch(_ConditionBranch),
-    Node(_ConditionNode),
-    End(_ConditionEnd),
-}
-
-#[derive(Deserialize)]
-pub struct _ConditionBranch {
-    pub clause: String,
-    pub excluded_routes: Vec<String>,
-    pub conditions: Vec<_Condition>,
-}
-
-#[derive(Deserialize)]
-pub struct _ConditionNode {
-    pub objective_id: String,
-    pub excluded_routes: Vec<String>,
-}
-
-#[derive(Serialize)]
-pub struct _RouteInstance {
-    pub info: _RouteInfo,
-    pub ordered_objectives: Vec<_ObjectiveInfo>,
-}
-
-#[derive(Deserialize, Serialize, Clone)]
-pub struct _RouteInfo {
-    pub name: String,
-    pub description: String,
-}
-
-#[derive(Deserialize)]
-pub struct _ConditionEnd;
-
-#[derive(Serialize)]
-pub struct _GeneratedRoute {
-    pub app_version: String,
-    pub game_name: String,
-    pub info: _RouteInfo,
-    pub seed: u64,
-    pub ordered_objectives: Vec<_ObjectiveInfo>,
-}
-
-#[derive(Deserialize)]
-pub struct Objectives {
-    pub items: Vec<Objective>,
-}
-
-#[derive(Deserialize, Serialize)]
-pub struct Objective {
-    pub name: String,
-    pub id: String,
-    pub labels: Vec<String>,
-    pub location: String,
-}
-
-#[derive(Deserialize)]
-pub struct Routes {
-    pub items: Vec<Route>,
+    pub routes: Vec<Route>,
+    pub objectives: Vec<Objective>,
 }
 
 #[derive(Deserialize)]
 pub struct Route {
     pub id: String,
     pub info: RouteInfo,
-    pub objectives: Vec<RouteObjective>,
+}
+
+#[derive(Deserialize, Serialize, Clone)]
+pub struct RouteInfo {
+    pub name: String,
+    pub description: String,
+}
+
+#[derive(Deserialize)]
+pub struct Objective {
+    pub id: String,
+    pub info: ObjectiveInfo,
+    pub excluded_routes: Vec<String>,
+    pub weighting: HashMap<String, u64>,
+    pub condition: Option<Condition>,
+}
+
+pub struct WeightedObjective {
+    pub id: String,
+    pub weight: u64,
+}
+
+#[derive(Deserialize, Serialize, Clone)]
+pub struct ObjectiveInfo {
+    pub name: String,
+    pub location: String,
 }
 
 #[derive(Deserialize)]
@@ -132,23 +62,22 @@ pub enum Condition {
 }
 
 #[derive(Deserialize)]
-pub struct RouteObjective {
-    pub id: String,
-    pub weight: u32,
-    pub labels: Vec<String>,
-    pub condition: Option<Condition>,
-}
-
-#[derive(Deserialize)]
 pub struct ConditionBranch {
     pub clause: String,
+    pub excluded_routes: Vec<String>,
     pub conditions: Vec<Condition>,
 }
 
 #[derive(Deserialize)]
 pub struct ConditionNode {
-    pub status: String,
     pub objective_id: String,
+    pub excluded_routes: Vec<String>,
+}
+
+#[derive(Serialize)]
+pub struct RouteInstance {
+    pub info: RouteInfo,
+    pub ordered_objectives: Vec<ObjectiveInfo>,
 }
 
 #[derive(Deserialize)]
@@ -156,7 +85,9 @@ pub struct ConditionEnd;
 
 #[derive(Serialize)]
 pub struct GeneratedRoute {
+    pub app_version: String,
+    pub game_name: String,
     pub info: RouteInfo,
     pub seed: u64,
-    pub objectives: Vec<Objective>,
+    pub ordered_objectives: Vec<ObjectiveInfo>,
 }
--- src/lib.rs
+++ src/lib.rs
@@ -16,7 +16,7 @@ pub fn ask_game_selection(games: &[Game]) -> &Game {
     &games[choice]
 }
 
-pub fn ask_route_selection(routes: &[_Route]) -> &_Route {
+pub fn ask_route_selection(routes: &[Route]) -> &Route {
     let choices: Vec<String> = routes
         .iter()
         .map(|route| route.info.name.clone())
@@ -42,7 +42,7 @@ pub fn load_route_schema(file_name: &String) -> Result<RouteSchema, Box<dyn std:
 
 fn possible_objectives_ids(
     route_id: &String,
-    objectives: &[_Objective],
+    objectives: &[Objective],
     completed: &[String]
 ) -> Vec<String> {
     let mut possible_objective_ids: Vec<String> = Vec::new();
@@ -66,21 +66,21 @@ fn possible_objectives_ids(
 
 fn resolve_condition(
     route_id: &String,
-    condition: &_Condition,
+    condition: &Condition,
     completed: &[String],
     total_objective_count: usize
 ) -> bool {
     match condition {
-        _Condition::Branch(branch) =>
+        Condition::Branch(branch) =>
             resolve_branch(route_id, branch, completed, total_objective_count),
-        _Condition::Node(node) => resolve_node(route_id, node, completed),
-        _Condition::End(_) => completed.len() == total_objective_count - 1,
+        Condition::Node(node) => resolve_node(route_id, node, completed),
+        Condition::End(_) => completed.len() == total_objective_count - 1,
     }
 }
 
 fn resolve_branch(
     route_id: &String,
-    branch: &_ConditionBranch,
+    branch: &ConditionBranch,
     completed: &[String],
     total_objective_count: usize
 ) -> bool {
@@ -108,7 +108,7 @@ fn resolve_branch(
     false
 }
 
-fn resolve_node(route_id: &String, node: &_ConditionNode, completed: &[String]) -> bool {
+fn resolve_node(route_id: &String, node: &ConditionNode, completed: &[String]) -> bool {
     // TODO: figure out refactor here as all condition types have excluded routes
     if node.excluded_routes.contains(route_id) {
         return false;
@@ -117,71 +117,12 @@ fn resolve_node(route_id: &String, node: &_ConditionNode, completed: &[String]) 
     completed.contains(&node.objective_id)
 }
 
-fn possible_objectives<'a>(
-    remaining: &'a [RouteObjective],
-    completed: &[String]
-) -> Vec<&'a RouteObjective> {
-    let mut possible: Vec<&RouteObjective> = Vec::new();
-
-    for route_objective in remaining {
-        if let Some(condition) = &route_objective.condition {
-            if condition_met(condition, completed, &remaining) {
-                possible.push(route_objective);
-            }
-        } else {
-            possible.push(route_objective);
-        }
-    }
-
-    possible
-}
-
-fn condition_met(
-    condition: &Condition,
-    completed: &[String],
-    remaining: &[RouteObjective]
-) -> bool {
-    match condition {
-        Condition::Branch(branch) => condition_branch_met(branch, completed, remaining),
-        Condition::Node(node) => condition_node_met(node, completed),
-        Condition::End(_) => remaining.len() == 1,
-    }
-}
-
-fn condition_node_met(condition: &ConditionNode, completed: &[String]) -> bool {
-    if condition.status == "completed" {
-        return completed.iter().any(|objective_id| objective_id == &condition.objective_id);
-    }
-
-    false
-}
-
-fn condition_branch_met(
-    condition: &ConditionBranch,
-    completed: &[String],
-    remaining: &[RouteObjective]
-) -> bool {
-    if condition.clause == "all" {
-        return condition.conditions
-            .iter()
-            .all(|condition| condition_met(condition, completed, remaining));
-    }
-
-    if condition.clause == "any" {
-        return condition.conditions
-            .iter()
-            .any(|condition| condition_met(condition, completed, remaining));
-    }
-
-    false
-}
-
 pub fn gen_rng(seed: u64) -> ChaCha8Rng {
     ChaCha8Rng::seed_from_u64(seed)
 }
 
-pub fn filter_objectives<'a>(route_id: &String, objectives: Vec<_Objective>) -> Vec<_Objective> {
-    let mut filtered_objectives: Vec<_Objective> = Vec::new();
+pub fn filter_objectives(route_id: &String, objectives: Vec<Objective>) -> Vec<Objective> {
+    let mut filtered_objectives: Vec<Objective> = Vec::new();
 
     for objective in objectives {
         if objective.excluded_routes.contains(route_id) {
@@ -196,11 +137,11 @@ pub fn filter_objectives<'a>(route_id: &String, objectives: Vec<_Objective>) -> 
 
 pub fn generate_ordered_objectives(
     route_id: &String,
-    objectives: &[_Objective],
+    objectives: &[Objective],
     rng: &mut ChaCha8Rng
-) -> Vec<_ObjectiveInfo> {
+) -> Vec<ObjectiveInfo> {
     let mut completed: Vec<String> = Vec::new();
-    let mut ordered_objectives: Vec<_ObjectiveInfo> = Vec::new();
+    let mut ordered_objectives: Vec<ObjectiveInfo> = Vec::new();
 
     while completed.len() < objectives.len() {
         let possible_objective_ids = possible_objectives_ids(route_id, objectives, &completed);
@@ -227,10 +168,10 @@ pub fn generate_ordered_objectives(
 fn build_weighted_objectives(
     route_id: &String,
     objective_ids: &[String],
-    objectives: &[_Objective]
-) -> Vec<_WeightedObjective> {
+    objectives: &[Objective]
+) -> Vec<WeightedObjective> {
     let default_weight: u64 = 1;
-    let mut weighted_objectives: Vec<_WeightedObjective> = Vec::new();
+    let mut weighted_objectives: Vec<WeightedObjective> = Vec::new();
 
     for objective_id in objective_ids {
         let objective = objectives
@@ -238,7 +179,7 @@ fn build_weighted_objectives(
             .find(|objective| objective.id == *objective_id)
             .unwrap();
 
-        weighted_objectives.push(_WeightedObjective {
+        weighted_objectives.push(WeightedObjective {
             id: objective.id.clone(),
             weight: objective.weighting.get(route_id).unwrap_or(&default_weight).clone(),
         });
@@ -248,7 +189,7 @@ fn build_weighted_objectives(
 }
 
 fn random_weighted_objective(
-    weighted_objectives: &[_WeightedObjective],
+    weighted_objectives: &[WeightedObjective],
     rng: &mut ChaCha8Rng
 ) -> String {
     let total = weighted_objectives
@@ -270,34 +211,14 @@ fn random_weighted_objective(
     }
 }
 
-fn random_objective_id<'a>(objectives: &'a [&RouteObjective], rng: &mut ChaCha8Rng) -> &'a String {
-    let total = objectives
-        .iter()
-        .map(|objective| objective.weight)
-        .sum::<u32>();
-
-    let mut roll = rng.gen_range(0..total);
-    let mut i = 0;
-
-    loop {
-        let objective = &objectives[i];
-        if roll < objective.weight {
-            return &objective.id;
-        }
-
-        roll -= objective.weight;
-        i += 1;
-    }
-}
-
 pub fn build_generated_route(
     app_version: String,
     game_name: String,
-    info: _RouteInfo,
+    info: RouteInfo,
     seed: u64,
-    ordered_objectives: Vec<_ObjectiveInfo>
-) -> _GeneratedRoute {
-    _GeneratedRoute {
+    ordered_objectives: Vec<ObjectiveInfo>
+) -> GeneratedRoute {
+    GeneratedRoute {
         app_version,
         game_name,
         info,
@@ -309,34 +230,11 @@ pub fn build_generated_route(
 pub fn write_generated_route(
     game_id: &String,
     route_id: &String,
-    generated_route: _GeneratedRoute
+    generated_route: GeneratedRoute
 ) -> Result<(), std::io::Error> {
     let time_sig = chrono::Local::now().format("%Y%m%d_%H%M%S").to_string();
     let file_name = format!("{}-{}-{}", game_id, route_id, time_sig);
     data::write_output(file_name, generated_route)
-}
-
-pub fn determine_objective_order(
-    mut objectives: Vec<RouteObjective>,
-    rng: &mut ChaCha8Rng
-) -> Vec<String> {
-    let mut order: Vec<String> = Vec::new();
-
-    while !objectives.is_empty() {
-        // Determine the next objective in the order
-        let possible = possible_objectives(&objectives, &order);
-        let next_objective_id = random_objective_id(&possible, rng);
-        order.push(next_objective_id.clone());
-
-        // Remove the objective from the remaining list
-        let index = objectives
-            .iter()
-            .position(|objective| objective.id == *next_objective_id)
-            .unwrap();
-        objectives.remove(index);
-    }
-
-    order
 }
 
 fn get_selection_index(question: String, choices: &[String]) -> usize {
@@ -371,40 +269,7 @@ pub fn get_seed() -> u64 {
     input.trim().parse().unwrap()
 }
 
-pub fn get_route_selection(mut routes: Routes) -> Route {
-    let choices: Vec<String> = routes.items
-        .iter()
-        .map(|route| route.info.name.clone())
-        .collect();
-    let choice = get_selection_index(String::from("Select a route"), &choices);
-    routes.items.remove(choice - 1)
-}
-
 pub fn get_game_selection(games: &[String]) -> String {
     let choice = get_selection_index(String::from("Select a game"), games);
     games[choice - 1].clone()
-}
-
-pub fn build_serialisable_route(
-    info: RouteInfo,
-    seed: u64,
-    mut objectives: Vec<Objective>,
-    objective_ids: &[String]
-) -> GeneratedRoute {
-    let mut ordered_objectives: Vec<Objective> = Vec::new();
-
-    for objective_id in objective_ids {
-        for (i, objective) in objectives.iter().enumerate() {
-            if objective.id == *objective_id {
-                ordered_objectives.push(objectives.remove(i));
-                break;
-            }
-        }
-    }
-
-    GeneratedRoute {
-        info,
-        seed,
-        objectives: ordered_objectives,
-    }
 }
